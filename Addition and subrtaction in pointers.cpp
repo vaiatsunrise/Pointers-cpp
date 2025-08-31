1d72c29b22f56d691aac90688d313c34f52166e1
Vaishnavi Verma <vaiatsunrise@gmail.com>
2025-08-31 22:51:16 +0530

    Add files via upload

---
+++ Addition and subrtaction in pointers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=9;
+    int b=5;
+    int*a1;
+    a1=&a;
+    int*b1;
+    b1=&b 
+    int sum,diff;
+    sum=*a1+*b1;
+    diff=*a1-*b1;
+    cout<<"Sum is: "<<sum<<endl;
+    cout<<"Difference is: "<<diff<<endl;
+
+}
+/*
+Output:
+Sum is: 14
+Difference is: 4
+*/
